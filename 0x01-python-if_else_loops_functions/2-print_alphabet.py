52291884fc9369e82d7d0152cb3f9af6a068722f
abdelhadia72 <thisisavnb@gmail.com>
2023-09-05 11:59:23 +0100

    fix 2 print alph

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    print(f"{chr(i)}", end="\n" if i == 122 else "")
+    print("{}".format(chr(i)), end="\n" if i == 122 else "")
+# print(chr(i), end="\n" if i == 122 else "")
