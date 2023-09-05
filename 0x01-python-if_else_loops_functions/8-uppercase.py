a567c10e1d1541dfeb4218f81bcb12106da7ccb9
abdelhadia72 <thisisavnb@gmail.com>
2023-09-05 12:43:51 +0100

    upp and low

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    holder = None
+    for i in str:
+        holder = i
+        if 97 <= ord(i) <= 122:
+            holder = chr(ord(i) - 32)
+        print(holder, end="")
+    print()
