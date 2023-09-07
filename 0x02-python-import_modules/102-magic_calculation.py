cd018f44f8bd2d63d1c0be578335086ab1f0f452
abdelhadia72 <thisisavnb@gmail.com>
2023-09-07 17:51:36 +0100

    try fix magic task

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,10 +3,8 @@ def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
 
     if a < b:
-        # print("{} + {} = {}".format(a, b, add(a, b)))
         c = add(a, b)
-        for i in range(90):
-            # print("{} + {} = {}".format(c, i, add(c, i)))
+        for i in range(89):
             c = add(c, i)
         return c
     else:
