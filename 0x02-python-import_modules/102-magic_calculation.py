e94cb2974d159c6bf0838d130dd1b3c49dd0da3f
abdelhadia72 <thisisavnb@gmail.com>
2023-09-07 17:47:02 +0100

    magic  task

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        # print("{} + {} = {}".format(a, b, add(a, b)))
+        c = add(a, b)
+        for i in range(90):
+            print("{} + {} = {}".format(c, i, add(c, i)))
+        return c
+    else:
+        return sub(a, b)
