cc5fe4fd62ecafd36b8da91dc888f70be9989d16
abdelhadia72 <thisisavnb@gmail.com>
2023-09-07 18:03:03 +0100

    refix task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
     else:
-        print("0 arguments.")
+        print("{} arguments.".format(len(sys.argv[1:])))
