948d28063fc056431c1147d05380597fba66d90f
abdelhadia72 <thisisavnb@gmail.com>
2023-09-09 22:54:32 +0100

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max = my_list[0] 
+    for i in range(1, len(my_list)):
+        if max < my_list[i]:
+            max = my_list[i]
+    return max
