0a1cbf34fea450bdf0f15a19f5b28ba7db7930d1
abdelhadia72 <thisisavnb@gmail.com>
2023-09-13 18:29:31 +0100

    uniq add without reduce

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,7 @@
 def uniq_add(my_list=[]):
     from functools import reduce
 
-    return reduce(lambda x, y: x + y, set(my_list))
+    s = 0
+    for i in set(my_list):
+        s += i
+    return s
