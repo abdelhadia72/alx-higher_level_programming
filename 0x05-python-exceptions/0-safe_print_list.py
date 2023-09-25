8390268b3dd8e63791502faa0b1993cc6ec91dab
abdelhadia72 <thisisavnb@gmail.com>
2023-09-25 10:34:38 +0100

    add 0 -> 2 tasks + readme

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+def safe_print_list(my_list, x):
+    i = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            i += 1
+        print("", end="\n")
+        return (i)
+    except IndexError:
+        return (i)
---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+def safe_print_integer(value):
+    try:
+        res = value/1
+    except TypeError:
+        return False
+    else:
+        print("{:.0f}".format(res))
+        return True
