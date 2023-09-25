3dfe4377482c895195e5c06d398e2a7dcf0c4354
abdelhadia72 <thisisavnb@gmail.com>
2023-09-25 11:34:15 +0100

    fixing task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except (ValueError, ZeroDivisionError):
         return (None)
     finally:
-        print("Inside result: {:.1f}".format(res))
+        print("Inside result: {:d}".format(res))
         return (res)
