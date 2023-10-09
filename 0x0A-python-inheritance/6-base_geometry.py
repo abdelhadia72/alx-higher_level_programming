14e7de47936fd245d7fb0b6602cfff645061cfa8
abdelhadia72 <thisisavnb@gmail.com>
2023-10-09 19:35:19 +0100

    Fix some errors

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 
+"""
+This file contains the BaseGeometry class.
+
+Methods:
+    area(self): Raises an exception indicating that the area() method is not implemented.
+"""
+
 
 class BaseGeometry:
     """
     This class represents a base geometry.
 
     Methods:
-        area(self): Raises an exception indicating
-        that the area() method is not implemented.
+        area(self): Raises an exception indicating that the area() method is not implemented.
     """
 
     def area(self):
         """
-        Raises an exception indicating that
-        the area() method is not implemented.
+        Raises an exception indicating that the area() method is not implemented.
         """
         raise Exception("area() is not implemented")
