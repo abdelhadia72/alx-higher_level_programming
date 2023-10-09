56d92dd5cc2b07867666a4d73ba42118d2e69f98
abdelhadia72 <thisisavnb@gmail.com>
2023-10-09 20:09:49 +0100

    Task 100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+class MyInt(int):
+    """
+    A custom integer class that overrides
+    the equality and inequality operators.
+    """
+
+    def __eq__(self, other):
+        """
+        Overrides the equality operator.
+        """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """
+        Overrides the inequality operator.
+        """
+        return super().__eq__(other)
