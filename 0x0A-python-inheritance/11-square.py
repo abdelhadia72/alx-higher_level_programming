6c8064cfaf2d909ff04c75bc88d014f2d9392b00
abdelhadia72 <thisisavnb@gmail.com>
2023-10-09 18:56:48 +0100

    Task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+This file contains the Square class.
+"""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    This is the Square class.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square object.
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        """
+        Returns a string representation of the Square object.
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
