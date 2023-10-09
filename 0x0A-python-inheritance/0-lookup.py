9e3b99cfcb39a9529784e6121d9742512a7882bb
abdelhadia72 <thisisavnb@gmail.com>
2023-10-09 16:07:07 +0100

    Task 0 add docstring

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+"""
+This script defines a function that returns a list of attributes
+and methods of an object.
+"""
+
 
 def lookup(obj):
     """Return a list of attributes
