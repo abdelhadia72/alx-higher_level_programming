7c5258e39b6a164eabc546ed83a6d6ff27126f50
abdelhadia72 <thisisavnb@gmail.com>
2023-10-10 23:56:27 +0100

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" read/write from file """
+
+
+class Student:
+    """
+    definitions of class Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """initialize Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to json method"""
+        if attrs is None:
+            return self.__dict__
+
+        new_dict = {}
+        for item in attrs:
+            try:
+                new_dict[item] = self.__dict__[item]
+            except Exception:
+                pass
+        return new_dict
