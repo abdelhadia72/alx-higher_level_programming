2b3ac4eb28165e5065a727558aabedb9b022716f
abdelhadia72 <thisisavnb@gmail.com>
2023-10-10 23:52:23 +0100

    Task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" read/write from file """
+
+
+def class_to_json(obj):
+    """unction that returns the dictionary
+    description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object"""
+    return obj.__dict__
