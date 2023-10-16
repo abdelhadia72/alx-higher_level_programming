d7ed2d58cf0e20646eb622c347ea6b8722c5a5c8
abdelhadia72 <thisisavnb@gmail.com>
2023-10-16 17:38:10 +0100

    fix style

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -144,7 +144,8 @@ class Base:
             writer = csv.writer(file)
             for obj in list_objs:
                 if cls.__name__ == "Rectangle":
-                    writer.writerow([obj.id, obj.width, obj.height, obj.x, obj.y])
+                    writer.writerow(
+                            [obj.id, obj.width, obj.height, obj.x, obj.y])
                 elif cls.__name__ == "Square":
                     writer.writerow([obj.id, obj.size, obj.x, obj.y])
 
