c0b311b315f4eaa1024e63be88e5fc8b463cfed1
abdelhadia72 <thisisavnb@gmail.com>
2023-12-11 18:15:38 +0100

    Fix task 4

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
 if (process.argv.length > 2) {
-  console.log(process.argv[2])
+  for (let i = 2; i < process.argv.length; i++) {
+    console.log(process.argv[i]);
+  }
 } else {
   console.log('No argument');
 }
