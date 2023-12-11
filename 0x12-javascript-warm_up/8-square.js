b855c65b654e4e3d6c2929ad0cde3cf8654849c8
abdelhadia72 <thisisavnb@gmail.com>
2023-12-11 23:56:00 +0100

    Fixing task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const numOfLoops = process.argv[2];
-if (process.argv[2] === undefined) {
-  console.log('Missing size');
-} else {
+const numOfLoops = parseInt(process.argv[2]);
+if (parseInt(process.argv[2])) {
   for (let i = 0; i < numOfLoops; i++) {
     console.log(('X').repeat(numOfLoops));
   }
+} else {
+  console.log('Missing size');
 }
