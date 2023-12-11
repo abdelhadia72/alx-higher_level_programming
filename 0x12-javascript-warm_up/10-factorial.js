47793157527337cd69092da1c22ad7bdb3e44d64
abdelhadia72 <thisisavnb@gmail.com>
2023-12-11 20:01:12 +0100

    Fix Task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+if (process.argv.length <= 3) {
+    console.log('0');
+} else {
+    const args = [];
+    for (let i = 2; i < process.argv.length; i++) {
+        args.push(process.argv[i]);
+    }
+    args.sort()
+    console.log(args[args.length - 2]);
+}
