98ea12c6cfbc5487200070b77d74d0cc84593022
abdelhadia72 <thisisavnb@gmail.com>
2023-12-11 23:44:08 +0100

    Fixing task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const numOfLoops = process.argv[2];
 if (process.argv[2] === undefined) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < numOfLoops; ++i) {
+  for (let i = 0; i < numOfLoops; i++) {
     console.log(('x').repeat(numOfLoops));
   }
 }
