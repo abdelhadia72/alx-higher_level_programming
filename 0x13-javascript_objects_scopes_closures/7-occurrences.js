288a928d4f2203bd7fa13a209a616680f691f3ba
abdelhadia72 <thisisavnb@gmail.com>
2023-12-13 00:59:41 +0100

    Task 7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,7 @@
+#!/bin/usr/node
+
+exports.nbOccurences = function (list, searchElement) {
+  return list.reduce(function (count, currentElement) {
+    return currentElement === searchElement ? count + 1 : count;
+  }, 0);
+};
