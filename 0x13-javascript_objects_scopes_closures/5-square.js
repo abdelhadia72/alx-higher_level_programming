5b9d55ce4d034e0d2169c516868e1948041ac22c
abdelhadia72 <thisisavnb@gmail.com>
2023-12-12 18:41:18 +0100

    Task 5

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+const Rectangle = require("./4-rectangle");
+
+module.exports = class Rectangle {
+    constructor(w, h) {
+        if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+    }
+
+    print() {
+        for (let i = 0; i < this.height; i++) {
+            console.log('X'.repeat(this.width));
+        }
+    }
+
+    rotate() {
+        const temp = this.width;
+        this.width = this.height;
+        this.height = temp;
+    }
+
+    double() {
+        this.width = this.width * 2;
+        this.height = this.height * 2;
+    }
+};
+
+
+module.exports = class Square extends Rectangle {
+    constructor(w, h, size) {
+        super(w, h);
+        this.size = size;
+    }
+}
