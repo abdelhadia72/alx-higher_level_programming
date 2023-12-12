fcb7610789e3cb64dd849bc4f49ef397160d3bd8
abdelhadia72 <thisisavnb@gmail.com>
2023-12-12 19:33:01 +0100

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+module.exports = class Square extends require('./4-rectangle') {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (char) {
+    if (char == undefined) {
+      this.print()
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(`${char}`.repeat(this.width));
+      }
+    }
+
+  }
+};
