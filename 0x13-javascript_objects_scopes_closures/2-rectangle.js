6ae286d273d70286a2dd97344b5dbf2e0500494b
abdelhadia72 <thisisavnb@gmail.com>
2023-12-12 17:07:59 +0100

    Task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,10 +4,13 @@
 */
 
 module.exports = class Rectangle {
-  constructor (w, h) {
+  constructor(w, h) {
     if (!(w <= 0 && h <= 0)) {
       this.width = w;
       this.height = h;
+    }
+    else {
+      return {}
     }
   }
 };
