e399b72f7f698b0bf4dd9056b8ee835cefb27fbc
abdelhadia72 <thisisavnb@gmail.com>
2023-12-12 17:18:51 +0100

    Task 2

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -4,7 +4,7 @@
 */
 
 module.exports = class Rectangle {
-  constructor (w, h) {
+  constructor(w, h) {
     this.width = w;
     this.height = h;
   }
--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
 
-/*
-  Rectangle Class that initializes height and width
-*/
-
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w < 0 || h < 0) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     } else {
       return {};
     }
   }
-};
+}
