d69a4fef00df5ee574f2605e379ac5129d043c04
abdelhadia72 <thisisavnb@gmail.com>
2023-12-13 02:56:27 +0100

    Fixing Task 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const { list } = require('./100-data');
-mapedArr = list.map((x, idx) => x * idx);
+const mapedArr = list.map((x, idx) => x * idx);
 
 console.log(list);
 console.log(mapedArr);
