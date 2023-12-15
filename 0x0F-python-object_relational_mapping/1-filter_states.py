23282786779503c1939dd4479d2e22bf3aaff1c7
abdelhadia72 <thisisavnb@gmail.com>
2023-12-15 11:18:27 +0100

    Fixing Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+'''
+    script that lists all states with a name starting
+    with N (upper N) from the database hbtn_0e_0_usa:
+'''
+
 import MySQLdb
 import sys
 
@@ -9,7 +14,8 @@ if __name__ == "__main__":
 
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database, charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
