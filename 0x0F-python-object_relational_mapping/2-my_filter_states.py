01920341e065e94c5f3cf49a42d596d40d852f5e
abdelhadia72 <thisisavnb@gmail.com>
2023-12-15 11:21:44 +0100

    Fixing Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+'''
+    script that takes in an argument and displays
+    all values in the states table of
+    hbtn_0e_0_usa where name matches the argument
+'''
+
 import MySQLdb
 import sys
 
@@ -7,9 +13,11 @@ if __name__ == "__main__":
     if len(sys.argv) != 5:
         exit(1)
 
-    username, password, database, keyword = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    username, password = sys.argv[1], sys.argv[2]
+    database, keyword = sys.argv[3], sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database, charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name = '{}'".format(keyword))
     query_rows = cur.fetchall()
