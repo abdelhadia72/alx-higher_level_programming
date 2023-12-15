f3e61eeab308408914cddbe2242715e4c1f8efa0
abdelhadia72 <thisisavnb@gmail.com>
2023-12-15 11:16:53 +0100

    Fixing task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 
+'''
+    List all states mathch the arguments db
+'''
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        sys.exit(1)
+        exit(1)
 
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states ORDER BY id ASC")
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-    except MySQLdb.Error as e:
-        print("Error: {}".format(e))
-        sys.exit(1)
-    finally:
-        if 'db' in locals() and db:
-            db.close()
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database, charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
