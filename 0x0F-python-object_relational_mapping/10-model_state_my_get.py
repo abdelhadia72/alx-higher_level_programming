840608072de2280f6d1072474819a741d9c979d1
abdelhadia72 <thisisavnb@gmail.com>
2023-12-16 21:20:51 +0100

    Adding Task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+python file that contains the class definition
+of a State and an instance Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class inherits from Base"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+    def __repr__(self):
+        return f"{self.id}: {self.name}"
+
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    import sys
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]
+    ), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    data = session.query(State).filter(State.name == sys.argv[4]).first()
+    if data is not None:
+        print(data.id)
+    else:
+        print("Not found")
