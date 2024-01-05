0110783f65514285360796cd1d13e058d4b86a68
abdelhadia72 <thisisavnb@gmail.com>
2024-01-05 17:19:06 +0100

    Task 0 fixing (02)

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Get length of body of a request
 
-curl -sI "$1" | grep -i "Content-Length:" | awk '{print $2}'
+if [ $# -ge 1 ]; then
+  curl -sI "$1" | grep -i "Content-Length:" | awk '{print $2}'
+fi
