d4e754fd6ae415586f0be9f38adbf7af024eb412
abdelhadia72 <thisisavnb@gmail.com>
2024-01-07 20:14:34 +0100

    Task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+This script retrieves the value of
+the 'X-Request-Id' header from a given URL.
+"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    reqest = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(reqest) as response:
+        print(response.headers.get('X-Request-Id'))
