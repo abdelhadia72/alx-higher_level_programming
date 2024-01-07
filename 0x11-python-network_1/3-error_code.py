ba375057fc25b47a3afea5a2c30bef49dd4159b2
abdelhadia72 <thisisavnb@gmail.com>
2024-01-07 20:40:10 +0100

    Task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""commend here"""
+
+import urllib.parse
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
