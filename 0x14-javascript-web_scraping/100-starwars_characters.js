37bd184e131614efcb94aa4bcda4de5d2cdcc876
abdelhadia72 <thisisavnb@gmail.com>
2024-01-30 17:52:04 +0100

    Task 100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,12 +1,35 @@
 #!/usr/bin/node
-/* fetch all user for a specific 
- * move based on the id 
+/* fetch all user for a specific
+ * move based on the id
  */
 
 const request = require('request');
-const base = 'https://swapi-api.alx-tools.com/api/'
+const base = 'https://swapi-api.alx-tools.com/api/';
 
-if (process.argv.length < 3){
-    console.error(`Usage: ./${process.argv[1]} <ID>`)
+if (process.argv.length < 3) {
+  console.error(`Usage: ./${process.argv[1]} <ID>`);
 }
 
+const movieUrl = `${base}films/${process.argv[2]}`;
+request.get(movieUrl, (err, res) => {
+  if (err) {
+    console.error('Error:', err.message);
+  }
+
+  const data = JSON.parse(res.body).characters;
+
+  const ids = data.map((char) => {
+    const id = (char.split('/'))[(char.split('/').length - 2)];
+    return id;
+  });
+
+  ids.forEach((id) => {
+    const charactersId = `${base}people/${id}`;
+    request.get(charactersId, (err, res) => {
+      if (err) {
+        console.error('Error:', err.message);
+      }
+      console.log(JSON.parse(res.body).name);
+    });
+  });
+});
