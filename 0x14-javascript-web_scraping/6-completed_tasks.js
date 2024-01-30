f6b7eb3e61c3a8ccbfbe3a8df65ba7dcd7e0a591
abdelhadia72 <thisisavnb@gmail.com>
2024-01-30 17:11:12 +0100

    Task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+/* print user withe there
+ * complted tasks
+ */
+
+const request = require('request');
+
+if (process.argv.length < 3) {
+  console.error(`Usage: ./${process.argv[1]} <URL> <FILE_PATH>`);
+  process.exit(1);
+}
+
+const url = process.argv[2];
+
+request.get(url, (err, res) => {
+  if (err) {
+    console.error('Error:', err.message);
+  }
+  const taskObj = {};
+  const data = JSON.parse(res.body);
+  data.map((obj) => {
+    if (!(taskObj[`${obj.userId}`])) { taskObj[`${obj.userId}`] = 0; }
+    if (obj.completed) { taskObj[`${obj.userId}`] = taskObj[`${obj.userId}`] + 1; }
+    return taskObj;
+  });
+  console.log(taskObj);
+});
