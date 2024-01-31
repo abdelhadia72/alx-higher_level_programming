285818a896ba46755ba6ab3fea34b7d23e776019
abdelhadia72 <thisisavnb@gmail.com>
2024-01-31 01:10:47 +0100

    fixing task 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,44 +1,39 @@
 #!/usr/bin/node
 /* fetch all user for a specific
- * movie based on the id and printed
- * by order
+ * move based on the id
  */
 
 const request = require('request');
 const base = 'https://swapi-api.alx-tools.com/api/';
 
 if (process.argv.length < 3) {
-    console.error(`Usage: ./${process.argv[1]} <ID>`);
+  console.error(`Usage: ./${process.argv[1]} <ID>`);
 }
 
 const movieUrl = `${base}films/${process.argv[2]}`;
-request.get(movieUrl, (err, res) => {
-    if (err) {
-        console.error('Error:', err.message);
-    }
+request.get(movieUrl, async (err, res) => {
+  if (err) {
+    console.error('Error:', err.message);
+  }
 
-    const data = JSON.parse(res.body).characters;
+  const data = JSON.parse(res.body).characters;
 
-    const ids = data.map((char) => {
-        const id = (char.split('/'))[(char.split('/').length - 2)];
-        return id;
+  const ids = data.map((char) => {
+    const id = (char.split('/'))[(char.split('/').length - 2)];
+    return id;
+  });
+
+  for (const id of ids) {
+    const charactersId = `${base}people/${id}`;
+    await new Promise((resolve, reject) => {
+      request.get(charactersId, (err, res) => {
+        if (err) {
+          console.error('Error:', err.message);
+          reject(err);
+        }
+        console.log(JSON.parse(res.body).name);
+        resolve();
+      });
     });
-    console.log(ids)
-
-    const idList = [];
-    let completedRequests = 0;
-    ids.forEach((id) => {
-        const charactersId = `${base}people/${id}`;
-        request.get(charactersId, (err, res) => {
-            if (err) {
-                console.error('Error:', err.message);
-            }
-            // idList.push(JSON.parse(res.body).name);
-            // completedRequests++;
-            // if (completedRequests === ids.length) {
-            //     console.log(idList.sort().reverse());
-            // }
-            console.log(JSON.parse(res.body).name);
-        });
-    });
+  }
 });
