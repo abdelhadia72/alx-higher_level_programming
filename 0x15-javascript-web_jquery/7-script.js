c5d54551b9b7ca089b30695c08aec36dcb14a246
abdelhadia72 <thisisavnb@gmail.com>
2024-02-07 17:40:08 +0100

    All tasks mandatory are done

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
         method: 'GET',
         success: function(data) {
-            $('#character').text(data.name);
+            $('DIV#character').text(data.name);
         }
     });
 });
---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $.ajax({
+        type: "GET",
+        url: "https://swapi-api.alx-tools.com/api/films/?format=json",
+        success: function (data) {
+            data.results.forEach(function(movie) {
+                $('UL#list_movies').append('<li>' + movie.title + '</li>');
+            });
+        }
+    });
+})
+
